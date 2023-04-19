19372f9176edff6017500a3f3e49449cc2ea226f
Sahand_MG <s23.moghadam@gmail.com>
2023-04-19 11:26:56 +0330

    adding log to http

--- app/Services/Http.php
+++ app/Services/Http.php
@@ -24,10 +24,10 @@ class Http
                 'Cookie: session=MTY4MTg4NTA3NnxEdi1CQkFFQ180SUFBUkFCRUFBQVpmLUNBQUVHYzNSeWFXNW5EQXdBQ2t4UFIwbE9YMVZUUlZJWWVDMTFhUzlrWVhSaFltRnpaUzl0YjJSbGJDNVZjMlZ5XzRNREFRRUVWWE5sY2dIX2hBQUJBd0VDU1dRQkJBQUJDRlZ6WlhKdVlXMWxBUXdBQVFoUVlYTnpkMjl5WkFFTUFBQUFHUC1FRlFFQ0FRUnliMjkwQVFwVGN6UTBOalEwT0RNeEFBPT18NfrDNr3fDKGSucgUHuB6XnrO8TD0MzCW_bfUi0DMvF8='
             ),
         ));
-
         $response = curl_exec($curl);
-
+        info('x-ui response: ');
+        info(json_encode($response));
         curl_close($curl);
-        echo $response;
+
     }
 }
