7457a8f301ef3be7bea72f0f1935328b80182f6e
Sahand_MG <s23.moghadam@gmail.com>
2023-07-07 00:58:38 +0330

    some improvements in introduce command

--- app/Console/Commands/IntroduceCommand.php
+++ app/Console/Commands/IntroduceCommand.php
@@ -7,37 +7,28 @@ use Illuminate\Console\Command;
 
 class IntroduceCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+
     protected $signature = 'server:introduce';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'send availability packet to master every 10 min';
+    public $counter = 0;
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
-        Http::sendHttp(config('bot.introduce_url'), ['server' => env('SERVER_ID')]);
+        $this->_sendRequest();
+    }
+
+    private function _sendRequest()
+    {
+        $resp = Http::sendHttp(config('bot.introduce_url'), ['server' => env('SERVER_ID')]);
+        $this->counter += 1;
+        if ($resp->status != 200 && $this->counter <= 3) {
+            sleep(3);
+            $this->_sendRequest();
+        }
     }
 }
