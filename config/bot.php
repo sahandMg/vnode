ba5c92866bb27739e6317dd42681022132957a3f
Sahand_MG <s23.moghadam@gmail.com>
2023-07-11 13:28:35 +0330

    change bot urls from http to https

--- config/bot.php
+++ config/bot.php
@@ -2,11 +2,11 @@
 
 return [
     'expire_after' => 2, // in minutes
-    'bot_node_exp' => 'http://pay.joyvpn.fun/api/send-notif-exp',
-    'bot_node_vol' => 'http://pay.joyvpn.fun/api/send-notif-vol',
+    'bot_node_exp' => 'https://pay.joyvpn.fun/api/send-notif-exp',
+    'bot_node_vol' => 'https://pay.joyvpn.fun/api/send-notif-vol',
     'login_url' => 'http://127.0.0.1:5529/login',
     'update_url' => 'http://127.0.0.1:5529/xui/inbound/update/',
-    'extra_inbounds_url' => 'http://pay.joyvpn.fun/api/send-extra-inbounds',
-    'interruption_url' => 'http://pay.joyvpn.fun/api/send-server-status',
-    'introduce_url' => 'http://pay.joyvpn.fun/api/send-server-introduce',
+    'extra_inbounds_url' => 'https://pay.joyvpn.fun/api/send-extra-inbounds',
+    'interruption_url' => 'https://pay.joyvpn.fun/api/send-server-status',
+    'introduce_url' => 'https://pay.joyvpn.fun/api/send-server-introduce',
 ];
