ec40eeef6ddbf7aa030a5c73bf4e1d5fbde8b224
Sahand Moghadam <sahand.mg.ne@gmail.com>
2024-03-04 10:01:20 +0330

    modifing direct introduce

--- app/Console/Commands/IntroduceCommand.php
+++ app/Console/Commands/IntroduceCommand.php
@@ -25,6 +25,21 @@ class IntroduceCommand extends Command
 
     private function _sendRequest()
     {
+        $resp = Http::sendHttp(config('bot.introduce_url'), ['server' => env('SERVER_ID')]);
+        $this->counter += 1;
+        if (gettype($resp) == 'boolean' || is_null($resp)) {
+            if ($this->counter <= 3) {
+                sleep(1);
+                $this->_sendRequest();
+            } else {
+                Log::info('Introducing to the master node failed after 3 times');
+            }
+        } else {
+            if ($resp->status != 200 && $this->counter <= 3) {
+                sleep(1);
+                $this->_sendRequest();
+            }
+        }
         $res = shell_exec('x-ui status');
         if (!str_contains($res, 'active')) {
             $url = config('bot.interruption_url');
