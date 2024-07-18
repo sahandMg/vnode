6623a81b0881cf7a206844c03371dadab53167f0
sahandmg <s23.moghadam@gmail.com>
2024-07-18 17:37:58 +0330

    changing to new iclland

--- config/bot.php
+++ config/bot.php
@@ -2,12 +2,12 @@
 
 return [
     'expire_after' => 2, // in minutes
-    'bot_node_exp' => 'https://pay.joyvpn.fun/api/send-notif-exp',
-    'bot_node_vol' => 'https://pay.joyvpn.fun/api/send-notif-vol',
+    'bot_node_exp' => 'https://pay.iclland.ir/api/send-notif-exp',
+    'bot_node_vol' => 'https://pay.iclland.ir/api/send-notif-vol',
     'login_url' => 'http://127.0.0.1:5529/login',
     'update_url' => 'http://127.0.0.1:5529/panel/inbound/updateClient/',
-    'extra_inbounds_url' => 'https://pay.joyvpn.fun/api/send-extra-inbounds',
-    'interruption_url' => 'https://pay.joyvpn.fun/api/send-server-status',
-    'introduce_url' => 'https://pay.joyvpn.fun/api/send-server-introduce',
+    'extra_inbounds_url' => 'https://pay.iclland.ir/api/send-extra-inbounds',
+    'interruption_url' => 'https://pay.iclland.ir/api/send-server-status',
+    'introduce_url' => 'https://pay.iclland.ir/api/send-server-introduce',
     'common_remark' => 'br1000',
 ];
